a426c8f0fd32a06cf35302412432507ea3e97f41
AbbasSalloum <Salloum916@gmail.com>
2023-12-26 16:58:58 -0500

    Add imports

--- PasswordManager.java
+++ PasswordManager.java
@@ -1,9 +1,26 @@
-
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.security.SecureRandom;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.AlgorithmParameterSpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+import javax.crypto.*;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEPar
 
 
 
 
 class SplashScreen{
     JFrame frame;
-    
+    JLabel image = new JLabel(ImageIcon("key-lock.png"));
 }
